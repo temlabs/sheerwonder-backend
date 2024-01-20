d11919ec3cbe8b9d52acf798ad633264ed1c678d
temlabs <temilabinjo@hotmail.co.uk>
2024-01-20 22:55:40 +0000

    can check whether a user exists

--- index.js
+++ index.js
@@ -4,14 +4,53 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fastify_1 = __importDefault(require("fastify"));
+const postgres_1 = __importDefault(require("@fastify/postgres"));
+const searchUsers_1 = require("./src/auth/stytch/searchUsers");
 require("dotenv").config();
 const server = (0, fastify_1.default)();
 const port = process.env.PORT || 3000;
+server.register(postgres_1.default, {
+    connectionString: process.env.DATABASE_URL,
+});
 server.get("/", async (request, reply) => {
-    return "hello world!\n";
+    return "hello world hiiii!\n";
 });
 server.get("/ping", async (request, reply) => {
     return "pong\n";
+});
+server.get("/names", async (request, reply) => {
+    try {
+        const dbClient = await server.pg.connect();
+        const { rows } = await dbClient.query("SELECT * FROM test");
+        dbClient.release();
+        return rows;
+    }
+    catch (err) {
+        console.error(err);
+        reply.status(500).send("Error querying the database");
+    }
+});
+server.get("/userExists", searchUsers_1.getUsersOptions, async (request, reply) => {
+    var _a;
+    try {
+        const { email = "", username = "" } = request.query;
+        const { results: users } = await (0, searchUsers_1.searchUsers)([
+            (0, searchUsers_1.createdBeforeNowFilter)(),
+            ...(0, searchUsers_1.emailAddressAndNameFilter)({ email, username }),
+        ]);
+        if (users.length === 0) {
+            return false;
+        }
+        const user = users[0];
+        if (username) {
+            return ((_a = user.name) === null || _a === void 0 ? void 0 : _a.first_name) === username.replaceAll("@", "");
+        }
+        return !!user;
+    }
+    catch (err) {
+        console.error(err);
+        reply.status(500).send("Error querying the database");
+    }
 });
 server.listen({
     port: port,
--- index.ts
+++ index.ts
@@ -1,17 +1,66 @@
 import fastify from "fastify";
+import fastifyPostgress from "@fastify/postgres";
+import {
+  GetUsersSchema,
+  createdBeforeNowFilter,
+  emailAddressAndNameFilter,
+  getUsersOptions,
+  searchUsers,
+} from "./src/auth/stytch/searchUsers";
 
 require("dotenv").config();
 
 const server = fastify();
 const port = (process.env.PORT as unknown as number) || 3000;
 
+server.register(fastifyPostgress, {
+  connectionString: process.env.DATABASE_URL,
+});
+
 server.get("/", async (request, reply) => {
-  return "hello world!\n";
+  return "hello world hiiii!\n";
 });
 
 server.get("/ping", async (request, reply) => {
   return "pong\n";
 });
+
+server.get("/names", async (request, reply) => {
+  try {
+    const dbClient = await server.pg.connect();
+    const { rows } = await dbClient.query("SELECT * FROM test");
+    dbClient.release();
+    return rows;
+  } catch (err) {
+    console.error(err);
+    reply.status(500).send("Error querying the database");
+  }
+});
+
+server.get<{ Querystring: GetUsersSchema }>(
+  "/userExists",
+  getUsersOptions,
+  async (request, reply) => {
+    try {
+      const { email = "", username = "" } = request.query;
+      const { results: users } = await searchUsers([
+        createdBeforeNowFilter(),
+        ...emailAddressAndNameFilter({ email, username }),
+      ]);
+      if (users.length === 0) {
+        return false;
+      }
+      const user = users[0];
+      if (username) {
+        return user.name?.first_name === username.replaceAll("@", "");
+      }
+      return !!user;
+    } catch (err) {
+      console.error(err);
+      reply.status(500).send("Error querying the database");
+    }
+  }
+);
 
 server.listen(
   {
