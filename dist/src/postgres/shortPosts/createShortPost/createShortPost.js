1a74bd75f6d03aaf177e5e9f01591886aaf3da88
temlabs <temilabinjo@hotmail.co.uk>
2024-07-21 00:16:00 +0100

    corrected create post query

--- dist/src/postgres/shortPosts/createShortPost/createShortPost.js
+++ dist/src/postgres/shortPosts/createShortPost/createShortPost.js
@@ -12,19 +12,20 @@ const createShortPost = async (dbClient, shortPost) => {
     const createdTrack = trackRows[0];
     console.debug("created track: ", createdTrack);
     const query = `
-      WITH inserted_post AS (
-        INSERT INTO short_posts (user_id, text, track_id, time_in, time_out, ext_id)
-        VALUES ($1, $2, $3, $4, $5, $6)
-        RETURNING *
-      )
-      SELECT 
-        p.id, p.name, p.artwork, p.text, p.time_in AS "timeIn", p.time_out AS "timeOut",
-        p.ext_id AS "extId", p.created_at AS "createdAt", p.user_id AS "userId",
-        p.spotify_id AS "spotifyId", p.save_count AS "saveCount", p.reply_count AS "replyCount",
-        p.upvote_count AS "upvoteCount",
-        u.username, u.avatar_url AS "avatarUrl", u.display_name AS "displayName"
-      FROM inserted_post p
-      JOIN users u ON p.user_id = u.id
+  WITH inserted_post AS (
+    INSERT INTO short_posts (user_id, text, track_id, time_in, time_out, ext_id)
+    VALUES ($1, $2, $3, $4, $5, $6)
+    RETURNING *
+  )
+  SELECT
+    p.id, t.name, t.artwork, p.text, p.time_in AS "timeIn", p.time_out AS "timeOut",
+    p.ext_id AS "extId", p.created_at AS "createdAt", p.user_id AS "userId",
+    t.spotify_id AS "spotifyId", p.save_count AS "saveCount", p.reply_count AS "replyCount",
+    p.upvote_count AS "upvoteCount",
+    u.username, u.avatar_url AS "avatarUrl", u.display_name AS "displayName"
+  FROM inserted_post p
+  JOIN users u ON p.user_id = u.id
+  JOIN tracks t ON p.track_id = t.id
     `;
     const values = [
         shortPost.userId,
