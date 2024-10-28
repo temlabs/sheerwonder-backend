fd07e8b9cb3ab0b4c1df479e5a87bdafea54c508
temlabs <temilabinjo@hotmail.co.uk>
2024-10-28 01:17:24 +0000

    reverting scopes

--- src/spotify/config.ts
+++ src/spotify/config.ts
@@ -1,6 +1,5 @@
 export const SPOTIFY_ACCESS_TOKEN_STALE_TIME = 3300 * 1000;
 export const SPOTIFY_BASE_URL = "https://accounts.spotify.com";
 export const SPOTIFY_PLAYER_BASE_URL = "https://api.spotify.com/v1/me/player";
-// export const SCOPE =
-//   "user-read-private user-modify-playback-state user-read-playback-state streaming playlist-read-private user-read-recently-played";
-export const SCOPE = "user-read-playback-state";
+export const SCOPE =
+  "user-read-private user-modify-playback-state user-read-playback-state streaming playlist-read-private user-read-recently-played";
